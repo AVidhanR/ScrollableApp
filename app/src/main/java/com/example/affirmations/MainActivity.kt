058733373e0457b4e7f644b15e95f3f4ccef2d3f
A Vidhan Reddy <itsvidhanreddy@gmail.com>
2023-11-08 14:31:50 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.affirmations
 
 import android.os.Bundle
@@ -102,5 +88,6 @@ fun AffirmationList(affirmationList: List<Affirmation>, modifier : Modifier = Mo
 @Composable
 @Preview
 private fun AffirmationsCardPreview() {
+    // using Affirmation data class
     AffirmationCard(Affirmation(R.string.affirmation1, R.drawable.image1))
 }
